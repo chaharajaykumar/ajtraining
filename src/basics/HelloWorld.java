06800dd63aca565dfeacf0710c6026cef325f40e
chaharajaykumar <104183960+chaharajaykumar@users.noreply.github.com>
2022-04-22 14:23:12 +0530

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 public static void main(String[] args){
 System.out.println("Hello world");
 System.out.println("im adding code to my project");
+  System.out.println("im adding code to my project from github");
 }
 }
